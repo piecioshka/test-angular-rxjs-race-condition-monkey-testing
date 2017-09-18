f65d99ba3856916d6082a900891cee3fac397c0d
Piotr Kowalski <piecioshka@gmail.com>
2017-09-18 17:47:16 +0200

    Solution 2: Use fromEvent operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { Article } from './article.interface';
 import { ArticlesService } from './articles.service';
+
+import * as Rx from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   articles: Array<Article>;
+
+  @ViewChild('button') $button;
 
   constructor(private articlesService: ArticlesService) {
     this.articles = [];
   }
 
-  onClickHandler() {
-    this.articles = [];
-
-    this.articlesService.fetchList()
+  ngOnInit() {
+    Rx.Observable
+      .fromEvent(this.$button.nativeElement, 'click')
+      .do(() => {
+        this.articles = [];
+      })
+      .switchMap(() => {
+        return this.articlesService.fetchList();
+      })
       .subscribe((list: Array<Article>) => {
         this.articles = list;
       });
