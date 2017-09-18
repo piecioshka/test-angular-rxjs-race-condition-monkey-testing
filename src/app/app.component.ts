d1f2f89122d9da67c8653b9c363652dd92440b5d
Piotr Kowalski <piecioshka@gmail.com>
2017-09-18 18:00:06 +0200

    Solution 3: Use Subject and cast it to Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Article } from './article.interface';
 import { ArticlesService } from './articles.service';
+
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   articles: Array<Article>;
+
+  private trigger = new Subject();
+
+  private clickStream$ = this.trigger
+    .asObservable()
+    .switchMap(() => this.articlesService.fetchList());
 
   constructor(private articlesService: ArticlesService) {
     this.articles = [];
   }
 
-  onClickHandler() {
-    this.articles = [];
-
-    this.articlesService.fetchList()
+  ngOnInit() {
+    this.clickStream$
       .subscribe((list: Array<Article>) => {
         this.articles = list;
       });
+  }
+
+  onClickHandler() {
+    this.articles = [];
+    this.trigger.next();
   }
 }
